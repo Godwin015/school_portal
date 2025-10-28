a4c13d56eae28a8906be70f0788555662db0040e
Godwin Edward <godwynedward015@gmail.com>
2025-10-28 16:17:36 +0100

    Update urls.py

--- payments/urls.py
+++ payments/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('initialize/', views.initialize_payment, name='initialize_payment'),
     path('verify/', views.verify_payment, name='verify_payment'),
     path('receipt/<str:reference>/', views.download_receipt, name='download_receipt'),
+    path('about/', views.about, name='about'),
+
 ]
+
