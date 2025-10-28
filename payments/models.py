649b7909a62c80f8928c2e5cf8678dbac5c938f6
Godwin Edward <godwynedward015@gmail.com>
2025-10-28 15:40:26 +0100

    Update models.py

---
+++ payments/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Payment(models.Model):
+    student_name = models.CharField(max_length=255)
+    session = models.CharField(max_length=20)
+    term = models.CharField(max_length=20)
+    parent_email = models.EmailField()
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    payment_reference = models.CharField(max_length=100, unique=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.student_name} - {self.payment_reference}"
