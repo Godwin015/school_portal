07bbc3610b70ea5313290009d30e8b2899d4f390
Godwin Edward <godwynedward015@gmail.com>
2025-10-31 06:33:59 +0100

    Update apps.py

--- payments/apps.py
+++ payments/apps.py
@@ -1,10 +1,5 @@
 from django.apps import AppConfig
 
 class PaymentsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'payments'
-
-    def ready(self):
-        from django.contrib import admin
-        admin.site.site_header = "Sunshine Academy Administration"
-        admin.site.site_title = "Sunshine Admin Portal"
-        admin.site.index_title = "Manage School Payments and Records"
